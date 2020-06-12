f30f3ea49b42ef78d047c7b33bf3fe8fadc10029
Jamie Liu <jamie.liu386@gmail.com>
2020-06-11 23:31:34 -0400

    Add functions for converting strings to numbers and vice versa

--- src/components/TextSlide/TextSlide.js
+++ src/components/TextSlide/TextSlide.js
@@ -4,13 +4,40 @@ import Slide from '@material-ui/core/Slide';
 import Grid from '@material-ui/core/Grid';
 import CountUp from 'react-countup';
 
+// function to convert a number to a string based on the given alphabet
+//    example: if alphabet is 'abc',
+//      0 -> 'a'
+//      1 -> 'b'
+//      2 -> 'c'
+//      3 -> 'aa'
+//      4 -> 'ab'
+//      5 -> 'ac'
+//      6 -> 'ba'
+//      ...
+function toLetters(num, alphabet) {
+  const mod = num % alphabet.length;
+  const pow = Math.floor(num / alphabet.length);
+  const out = alphabet[mod];
+  return pow ? toLetters(pow, alphabet) + out : out;
+}
+
+// function to convert a string to a number based on the given alphabet
+// see example for toLetters
+function fromLetters(str, alphabet) {
+  var out = 0, len = str.length, pos = len;
+  while (--pos > -1) {
+      out += (alphabet.indexOf(str[pos])) * Math.pow(alphabet.length, len - 1 - pos);
+  }
+  return out;
+}
+
 export default function TextSlide(props) {
   const passwordGuesser = (
     (props.usesInput !== true) ? null :
     (props.inputType === 'num') ? (
       <CountUp
         start={0}
-        duration={1/10000 * parseInt(props.userInput, 10)}
+        duration={parseInt(props.userInput, 10) / 10000}
         end={parseInt(props.userInput, 10)}
         formattingFn={num => String(num).padStart(props.inputLength, '0')}
         useEasing={false}>
